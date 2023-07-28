7ebd16efda359a3f9a69c18643e351ec7f09b148
Ravarotto Alfredo Alejandro <alfredo.ravarotto@globant.com>
2023-07-28 11:19:30 -0300

    Change logic and styles

--- src/services/getAllRecipes.js
+++ src/services/getAllRecipes.js
@@ -1,6 +1,9 @@
-export const getAllRecipes = async(setRecipes) => {
+export const getAllRecipes = async (setRecipes) => {
     const res = await fetch(
         "http://localhost:4000/v1/recipes",
+        {
+            method: "GET"
+        }
     );
     const data = await res.json();
     setRecipes(data);
